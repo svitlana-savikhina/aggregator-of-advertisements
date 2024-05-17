81824e76f54dd04c79c1d53be692c042d5cbf11d
Svitlana Savikhina <savihina111@gmail.com>
2024-05-17 18:39:32 +0300

    Add router to call the parser on user request

--- car_info/router.py
+++ car_info/router.py
@@ -1,11 +1,16 @@
 from datetime import datetime
 from typing import List
+from urllib.parse import urljoin
 
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
+from autoria_scraper.parse import get_advertisement_info, save_car_to_database
 from car_info import crud, schemas
 from dependencies import get_db
+
+BASE_URL = "https://auto.ria.com/uk/"
+HOME_URL = urljoin(BASE_URL, "auto_hyundai_tucson_34825798.html")
 
 router = APIRouter()
 
@@ -13,6 +18,13 @@ router = APIRouter()
 @router.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@router.get("/parse_car_info/")
+def parse_car_info():
+    car_item = get_advertisement_info()
+    saved_car = save_car_to_database(car_item)
+    return {"message": "Car information parsed and saved successfully!", "car_id": saved_car.id}
 
 
 @router.get("/car/{car_id}", response_model=schemas.Car)
@@ -24,7 +36,9 @@ def get_car(car_id: int, db: Session = Depends(get_db)):
 
 
 @router.get("/cars/", response_model=List[schemas.Car])
-def get_cars_by_period(start_date: datetime, end_date: datetime, db: Session = Depends(get_db)):
+def get_cars_by_period(
+        start_date: datetime, end_date: datetime, db: Session = Depends(get_db)
+):
     return crud.get_cars_by_period(db=db, start_date=start_date, end_date=end_date)
 
 
