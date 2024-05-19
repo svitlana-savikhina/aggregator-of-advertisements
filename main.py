6413465b79d8127ff9cd71d5beb06aa00ce3189f
Svitlana Savikhina <savihina111@gmail.com>
2024-05-19 15:53:37 +0300

    Add inclusion of user_router in main.py.

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-from fastapi import FastAPI
-from car_info import router as car_info_router
+from typing import Annotated
+
+from fastapi import FastAPI, Depends
+
+from user import routers as user_router
+from car_info import routers as car_info_router
+from user.auth import oauth2_scheme
 
 app = FastAPI()
 
 app.include_router(car_info_router.router)
+app.include_router(user_router.router)
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+
+@app.get("/items/")
+async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
+    return {"token": token}
